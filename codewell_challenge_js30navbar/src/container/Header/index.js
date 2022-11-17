2cbf160cce77cb4f46babdda52bfd9893f1f43f0
Devansh Agarwal <devanshagar783@gmail.com>
2022-11-17 13:46:03 +0530

    added some screen size adaptability

--- codewell_challenge_js30navbar/src/container/Header/index.js
+++ codewell_challenge_js30navbar/src/container/Header/index.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import "./index.css"
-import logo from "../../assets/Logo.svg"
+import React from "react";
+import "./index.css";
+import logo from "../../assets/Logo.svg";
+import menu from "../../assets/Menu.svg";
 
 function Header() {
-  return (
-    <div className='header_container'>
-      <div className='header_logo'>
-        <img src={logo} />
-      </div>
-      <div className='header_links'>
-        <p className='header_li_items'><a href="#">Products</a></p>
-        <p className='header_li_items'><a href="#">Challenges</a></p>
-        <p className='header_li_items'><a href="#">Resources</a></p>
-        <p className='header_li_items'><a href="#">Other Links</a></p>
-      </div>
-      <div className='header_sign'>
-        <p>Sign In</p>
-        <button type='button'>Try for free</button>
-      </div>
-    </div>
-  )
+    return (
+        <div className="header_container">
+            <div className="header_logo">
+                <img src={logo} />
+            </div>
+            <div className="header_links">
+                <p className="header_li_items">
+                    <a href="#">Products</a>
+                </p>
+                <p className="header_li_items">
+                    <a href="#">Challenges</a>
+                </p>
+                <p className="header_li_items">
+                    <a href="#">Resources</a>
+                </p>
+                <p className="header_li_items">
+                    <a href="#">Other Links</a>
+                </p>
+            </div>
+            <div className="header_sign">
+                <p>Sign In</p>
+                <button type="button">Try for free</button>
+            </div>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
