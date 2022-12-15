8278e1cd37ff97c0222cb49968f886c942ecd90c
Devansh Agarwal <devansh1.agarwal@paytmbank.com>
2022-12-15 23:52:20 +0530

    next project init

---
+++ yt-tut2-cart/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        Header here
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ yt-tut2-cart/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      You are ar 192.168.1.1
+    </div>
+  )
+}
+
+export default Home
