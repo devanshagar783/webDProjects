9862d2cfcca150c5e3fe959cfd1ae312741de859
Devansh Agarwal <devansh1.agarwal@paytmbank.com>
2023-01-19 00:48:13 +0530

    added delete functionality

--- new-proj/src/App.js
+++ new-proj/src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Done from "@mui/icons-material/Done";
 import Close from "@mui/icons-material/Close";
+import Delete from "@mui/icons-material/Delete";
 import { useState } from "react";
 
 function App() {
     const [text, setText] = useState("");
     const [todoList, setTodoList] = useState([]);
-    const [size,setSize] = useState(0)
+    const [size, setSize] = useState(0);
 
     const onDoneClick = () => {
-        setTodoList(oldList => [...oldList, {text:text, isChecked: false}]);
+        setTodoList((oldList) => [
+            ...oldList,
+            { text: text, isChecked: false },
+        ]);
         setText("");
     };
 
@@ -19,23 +23,55 @@ function App() {
         setText(e.target.value);
     };
 
-    const checkItem = (item) => {
-        const l=todoList.length
-        const index = todoList.indexOf(item);
-        setTodoList([{...todoList[index], isChecked: true }, ...todoList.slice(0, index), ...todoList.slice(index+1, l)])
+    const onDeleteItem = (item) => {
+        setTodoList(oldList => 
+            oldList.filter(curr => curr.text !== item.text)
+        )
+        setSize(size-1)
     }
+
+    const checkItem = (item) => {
+        const l = todoList.length;
+        const index = todoList.indexOf(item);
+        setTodoList([
+            { ...todoList[index], isChecked: true },
+            ...todoList.slice(0, index),
+            ...todoList.slice(index + 1, l),
+        ]);
+    };
 
     const CheckList = () => {
         let list = [];
-        if (todoList && size <= todoList.length) {list = [...todoList].reverse();setSize(todoList.length)}
-        else list = [...todoList];
+        if (todoList && size <= todoList.length) {
+            list = [...todoList].reverse();
+            setSize(todoList.length);
+        } else list = [...todoList];
         return (
             <div className="app_checklist">
                 {list.map((elem, index) => {
                     return (
-                        <div key={index} className="app_check_item">
-                            <input type={"checkbox"}  disabled={elem.isChecked} checked={elem.isChecked} value={elem.text} onChange={()=>{ checkItem(elem); console.log("elem cl",elem,index) }} />
-                            <label className={elem.isChecked ? "app_check_item_done" : ""}>{elem.text}</label>
+                        <div className="app_checkitem_container">
+                            <div key={index} className="app_check_item">
+                                <input
+                                    type={"checkbox"}
+                                    disabled={elem.isChecked}
+                                    checked={elem.isChecked}
+                                    value={elem.text}
+                                    onChange={() => {
+                                        checkItem(elem);
+                                    }}
+                                />
+                                <label
+                                    className={
+                                        elem.isChecked
+                                            ? "app_check_item_done"
+                                            : ""
+                                    }
+                                >
+                                    {elem.text}
+                                </label>
+                            </div>
+                            <Delete className="delete_icon" onClick={()=>{onDeleteItem(elem)}} />
                         </div>
                     );
                 })}
