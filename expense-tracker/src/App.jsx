2f23efc6dd9c35b0c088cc8ca3ca889d6b815fa9
Devansh Agarwal <devanshagar783@gmail.com>
2023-08-25 23:47:02 +0530

    minor changes

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <div className="">
+            <Navbar />
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ expense-tracker/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="h-24 flex flex-row bg-white text-black px-10 items-center">
+            <div className="">Expense Manager</div>
+        </div>
+    );
+};
+
+export default Navbar;
